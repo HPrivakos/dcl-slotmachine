c721b2201324b3360a9a24756d50a1a9c55ecbc8
kkuette <awakeproduction@hotmail.fr>
2019-01-27 20:22:55 +0100

    fixed slot rotation

--- client/src/modules/slot.ts
+++ client/src/modules/slot.ts
@@ -2,7 +2,7 @@
 export class SpinHandle {
     active: boolean = false
     fraction: number = 0
-    speed: number = 30
+    speed: number = 10000
     downpos: Vector3
     uppos: Vector3
     direction: Vector3 = Vector3.Backward()
@@ -92,6 +92,8 @@ export class Slot_Machine {
         this.buttom_2.add(new GLTFShape("models/Slot/Buttom_02.gltf"))
         this.buttom_3.add(new GLTFShape("models/Slot/Buttom_03.gltf"))
         this.spin_handle.add(new GLTFShape("models/Slot/SpinHandle.gltf"))
+
+        
     }
 
     move(){
@@ -99,10 +101,25 @@ export class Slot_Machine {
         position: this.position,
         scale: new Vector3(this.size, this.size, this.size)
       }))
-      this.wheel_1.add(new Transform({
-        position: new Vector3(this.position.x-0.05, this.position.y+1.582, this.position.z-0.3),
-        scale: new Vector3(this.size, this.size, this.size)
-      }))
+      
+      this.wheel_1.add(new Transform())
+      this.wheel_2.add(new Transform())
+      this.wheel_3.add(new Transform())
+
+      this.spin_handle.add(new Transform())
+
+      this.wheel_1.setParent(this.core)
+      this.wheel_2.setParent(this.core)
+      this.wheel_3.setParent(this.core)
+
+      this.spin_handle.setParent(this.core)
+
+      this.wheel_1.get(Transform).position.set(-0.13, 4.53, -0.86)
+      this.wheel_2.get(Transform).position.set(-0.13, 4.53, 0)
+      this.wheel_3.get(Transform).position.set(-0.13, 4.53, 0.86)
+
+      this.spin_handle.get(Transform).position.set(0, 4.33, 1.74)
+      /*
       this.wheel_2.add(new Transform({
         position: new Vector3(this.position.x-0.05, this.position.y+1.582, this.position.z),
         scale: new Vector3(this.size, this.size, this.size)
@@ -110,7 +127,7 @@ export class Slot_Machine {
       this.wheel_3.add(new Transform({
         position: new Vector3(this.position.x-0.05, this.position.y+1.582, this.position.z+0.3),
         scale: new Vector3(this.size, this.size, this.size)
-      }))
+      }))*/
       this.buttom_1.add(new Transform({
         position: this.position,
         scale: new Vector3(this.size, this.size, this.size)
@@ -123,21 +140,22 @@ export class Slot_Machine {
         position: this.position,
         scale: new Vector3(this.size, this.size, this.size)
       }))
+    /*
     this.spin_handle.set(new Transform({
         position: new Vector3(this.position.x, this.position.y+1.51, this.position.z+0.58),
         scale: new Vector3(this.size, this.size, this.size),
-    }));
+    }));*/
     }
 
     rotate(value: number){
       this.core.get(Transform).rotation = Quaternion.Euler(0, value, 0)
-      this.wheel_1.get(Transform).rotation = Quaternion.Euler(0, value, 0)
-      this.wheel_2.get(Transform).rotation = Quaternion.Euler(0, value, 0)
-      this.wheel_3.get(Transform).rotation = Quaternion.Euler(0, value, 0)
+      //this.wheel_1.get(Transform).rotation = Quaternion.Euler(0, value, 0)
+      //this.wheel_2.get(Transform).rotation = Quaternion.Euler(0, value, 0)
+      //this.wheel_3.get(Transform).rotation = Quaternion.Euler(0, value, 0)
       this.buttom_1.get(Transform).rotation = Quaternion.Euler(0, value, 0)
       this.buttom_2.get(Transform).rotation = Quaternion.Euler(0, value, 0)
       this.buttom_3.get(Transform).rotation = Quaternion.Euler(0, value, 0)
-      this.spin_handle.get(Transform).rotation = Quaternion.Euler(0, value, 0)
+      //this.spin_handle.get(Transform).rotation = Quaternion.Euler(0, value, 0)
     }
 
     show(){
@@ -150,5 +168,7 @@ export class Slot_Machine {
       engine.addEntity(this.buttom_2)
       engine.addEntity(this.buttom_3)
       engine.addEntity(this.spin_handle)
+
+      
     }
 }
